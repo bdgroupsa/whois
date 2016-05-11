ff3dc8e06968287d823841cd595e3063f333ec92
Goodwish M <net@cdn.businessdomains.co.za>
2016-05-11 15:32:34 +0200

    whois-update
    changes to syntax

--- bdgroup-api.js
+++ bdgroup-api.js
@@ -3,33 +3,33 @@
   License: MIT Common License 
   G. Matyila - 2016
   */
-		var Client = Client || {};
-		Client.Services = {
-			Responses: {
-				Available: "",//MESSAGE FOR AVAIL
-				Unavailable: "",//MESSAGE FOR UNAVAIL
-				OrderMessage: "" //ORDER MESSAGEE
-			}
-		}
-		function DataInfo(){
-			var xhttp = new XMLHttpRequest();
-			  xhttp.onreadystatechange = function(){
-				  if(xhttp.readyState == 4 && xhttp.status == 200){
-					  var DataResponse = JSON.parse(xhttp.responseText);
-						function Validator(){
-					 if(DataResponse.registered !== true){
-						    document.getElementById('DataSet').innerHTML = Client.Services.Responses.Available;
-						  document.getElementById('processOrder').value = document.getElementById('DomainChecker').value + Client.Services.Responses.OrderMessage; 
-					  }else{
-						 document.getElementById('DataSet').innerHTML = Client.Services.Responses.Unavailable;
-					  }
-						}
-						Validator();
-				  }else{
-					  console.log('process executed');
-				  }
-			  }
-			xhttp.open("get",'http://api.whoapi.com/?apikey=952c8451a7a5618274bea0185cbe1fbc&r=whois&domain=google.com', true );
-		  xhttp.send();	
-		}
-     /* Api ends */
+  
+
+//WHOIS REST CALL async 
+function queryProcessorWhoIs(){
+ var xquery = new XMLHttpRequest();
+  xquery.addEventListener('readystatechange',function(){
+	  if(xquery.readyState == 4 && xquery.status == 200){
+	  var $container = JSON.parse(xquery.responseText);
+	  if($container.registered !== true){
+		services.unregistered();
+	}else{
+	document.getElementById('DataSet').innerHTML = services.registered;
+	 }
+   }
+ });
+  xquery.open("GET",'http://api.whoapi.com/?apikey=952c8451a7a5618274bea0185cbe1fbc&r=whois&domain=www.google.com', true);
+ xquery.send();
+};
+
+//SERVICE RESPONSE FOR WHOIS
+var services = {
+	registered : "<img src='img/x.png' width='64px'>",
+	unregistered : (function(){
+		 setTimeout(function(){
+			 alert('will register it for you ');
+		 },5000);
+	})
+}	 
+	
+	
